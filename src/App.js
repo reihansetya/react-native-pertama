b6bccca6a8ec38f0860b26f0c75f86d82b66fd2a
reihansetya <reihanhanhan@gmail.com>
2021-08-06 15:58:18 +0700

    menambah class life

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import Flexbox from './pages/Flexbox';
 import Position from './pages/Position';
@@ -6,13 +6,19 @@ import SampleComponent from './pages/SampleComponent';
 import StylingComponent from './pages/StylingComponent';
 
 const App = () => {
+  const [isShow, SetIsShow] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     SetIsShow(false);
+  //   }, 10000);
+  // }, []);
   return (
     <View>
       <ScrollView>
-        <SampleComponent />
-        <StylingComponent />
-        <Flexbox />
-        <Position />
+        {/* <SampleComponent />
+        <StylingComponent /> */}
+        {isShow && <Flexbox />}
+        {/* <Position /> */}
       </ScrollView>
     </View>
   );
--- src/pages/Flexbox/index.js
+++ src/pages/Flexbox/index.js
@@ -3,7 +3,39 @@ import {Text, View, Image} from 'react-native';
 import poto from '../../assets/img/mackbook.jpg';
 
 class Flexbox extends Component {
+  constructor(props) {
+    super(props);
+    console.log('==> constructor');
+    this.state = {
+      subscriber: 1.2,
+      nama: 'Ludovico einaudi',
+      profil:
+        'https://yt3.ggpht.com/ytc/AKedOLS5VPsArmaKjvq0DPucpbPiqUppbU-db1LfjitXLg=s88-c-k-c0x00ffffff-no-rj',
+    };
+  }
+
+  componentDidMount() {
+    console.log('==> componentDidMount');
+    setTimeout(() => {
+      this.setState({
+        subscriber: 1.5,
+        nama: 'ambient studio',
+        profil:
+          'https://yt3.ggpht.com/ytc/AKedOLTS5YaA8mh82ojycWHu6urH6fhFK4NvDvS-pXZ5=s88-c-k-c0x00ffffff-no-rj',
+      });
+    }, 5000);
+  }
+
+  componentDidUpdate() {
+    console.log('==> component did update');
+  }
+
+  componentWillUnmount() {
+    console.log('==> component will unmount');
+  }
+
   render() {
+    console.log('==> render');
     return (
       <View>
         <View
@@ -20,16 +52,16 @@ class Flexbox extends Component {
         </View>
         <View style={{flexDirection: 'row', alignItems: 'center'}}>
           <Image
-            source={{
-              uri: 'https://yt3.ggpht.com/ytc/AKedOLS5VPsArmaKjvq0DPucpbPiqUppbU-db1LfjitXLg=s88-c-k-c0x00ffffff-no-rj',
-            }}
+            source={{uri: this.state.profil}}
             style={{width: 100, height: 100, borderRadius: 50, marginRight: 10}}
           />
           <View>
             <Text style={{fontWeight: 'bold', fontSize: 20}}>
-              Ludovico einaudi
+              {this.state.nama}
             </Text>
-            <Text style={{fontSize: 15}}>1,18jt subscriber</Text>
+            <Text style={{fontSize: 15}}>
+              {this.state.subscriber}jt subscriber
+            </Text>
           </View>
         </View>
         <View
@@ -39,7 +71,7 @@ class Flexbox extends Component {
             marginTop: 5,
           }}>
           <Text>Beranda</Text>
-          <Text>Video</Text>
+          <Text>Vidseo</Text>
           <Text>Playlist</Text>
           <Text>Komunitas</Text>
           <Text>Channel</Text>
